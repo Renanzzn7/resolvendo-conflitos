89e00d22c9985769f53a2dd9faafdfcfb51bd1ad
LAÍS ZANQUETA <lais.zanqueta@portalsesisp.org.br>
2026-03-06 11:48:27 -0300

    alunoB inserindo linha6 vazia e linha 7 com código

--- conflito.ts
+++ conflito.ts
@@ -2,3 +2,6 @@ console.log(`alunoA alterou a linha 1 do arquivo conflito.ts`);
 console.log(`alunoB inserindo a linha 2`);
 
 let calculo: number = (75 + 75) /3; //imaginando um calculo qualquer
+
+
+console.log(`Inserindo linha 7 pelo alunoB`);
